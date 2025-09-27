c15eebcd0e35df54a1b5a03e1d96473637a606c0
completo345 <joaco345vz@gmail.com>
2025-09-26 21:45:11 -0300

    home creado

--- casa_verde/settings.py
+++ casa_verde/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registro_user', 
+    'materiales',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#redireccion despues de iniciar secion y cerrar
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
