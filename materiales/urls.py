e07d551e1dbe66c3a094423bd9c38aff0bd26a36
JDMorenoS <jmorenos@usm.cl>
2025-11-10 20:34:44 -0300

    Linkeo del Boton MATERIALES a la pestaña de seleccion de provincia

--- materiales/urls.py
+++ materiales/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.elegir_provincia, name="materiales_index"),
     path("provincia/", views.elegir_provincia, name="elegir_provincia"),
     path("mis-datos/", views.datos_terreno, name="datos_terreno"),
 ]
 
 
-#solo espero que funcione 
+#solo espero que funcione
