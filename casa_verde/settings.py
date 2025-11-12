d3d7ad11f456ea2be0fa39d1afe1a4fc84612d2e
completo345 <joaco345vz@gmail.com>
2025-11-11 21:44:25 -0300

    ya se puede serrar secion

--- casa_verde/settings.py
+++ casa_verde/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 #redireccion despues de iniciar secion y cerrar
 LOGIN_URL='login'
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'usuarios'
+LOGOUT_REDIRECT_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
